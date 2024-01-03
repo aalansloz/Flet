cf0d4b803a53b86297f12d052c89a5ce2515525b
aalansloz <101714370+aalansloz@users.noreply.github.com>
2024-01-03 18:09:48 +0100

    last backup

--- Youtube_downloader/app.py
+++ Youtube_downloader/app.py
@@ -150,57 +150,19 @@ def main(page: ft.Page):
 
 #hacer que este listado sea el bueno leido del dataframe,
 # asi como añadir una opcion que me ejecute el code colab
-
+    path = os.getcwd()
+    path_full_feed=f'{path}//RSS'
+    feeds_file=pd.read_excel(f'{path_full_feed}//Feeds.xls')
+    #names is feeds_file['Name']
     card_list=ft.Card(
             content=ft.Container(
                 width=500,
                 content=ft.Column(
                     [
                         ft.ListTile(
-                            title=ft.Text("One-line list tile"),
-                        ),
-                        ft.ListTile(title=ft.Text("One-line dense list tile"), dense=True),
-                        ft.ListTile(
                             leading=ft.Icon(ft.icons.SETTINGS),
                             title=ft.Text("One-line selected list tile"),
                             selected=True,
-                        ),
-                        ft.ListTile(
-                            leading=ft.Image(src="/icons/icon-192.png", fit="contain"),
-                            title=ft.Text("One-line with leading control"),
-                        ),
-                        ft.ListTile(
-                            title=ft.Text("One-line with trailing control"),
-                            trailing=ft.PopupMenuButton(
-                                icon=ft.icons.MORE_VERT,
-                                items=[
-                                    ft.PopupMenuItem(text="Item 1"),
-                                    ft.PopupMenuItem(text="Item 2"),
-                                ],
-                            ),
-                        ),
-                        ft.ListTile(
-                            leading=ft.Icon(ft.icons.ALBUM),
-                            title=ft.Text("One-line with leading and trailing controls"),
-                            trailing=ft.PopupMenuButton(
-                                icon=ft.icons.MORE_VERT,
-                                items=[
-                                    ft.PopupMenuItem(text="Item 1"),
-                                    ft.PopupMenuItem(text="Item 2"),
-                                ],
-                            ),
-                        ),
-                        ft.ListTile(
-                            leading=ft.Icon(ft.icons.SNOOZE),
-                            title=ft.Text("Two-line with leading and trailing controls"),
-                            subtitle=ft.Text("Here is a second title."),
-                            trailing=ft.PopupMenuButton(
-                                icon=ft.icons.MORE_VERT,
-                                items=[
-                                    ft.PopupMenuItem(text="Item 1"),
-                                    ft.PopupMenuItem(text="Item 2"),
-                                ],
-                            ),
                         ),
                     ],
                     spacing=0,
